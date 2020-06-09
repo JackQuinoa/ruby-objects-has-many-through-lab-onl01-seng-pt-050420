5dd88805640257d3b74dbd5b9cdf2969053c3aa4
Jack Quinoa <jquinoa208@gmail.com>
2020-06-09 23:41:36 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -8,17 +8,11 @@ class Appointment
     @date = date
     @patient = patient
     @doctor = doctor
+    @@all  << self 
   end
   
   def self.all 
     @@all 
   end
   
-  # def patient
-  #   self.patient
-  # end
-  
-  # def doctor
-  #   self.doctor 
-  # end
 end
